e4b90a1ea8adbd6c49287db1965d8a50e0460b9a
ahmedsmoselhi <ahmedsmoselhi@gmail.com>
2026-03-17 07:44:34 +0200

    feat(auth):add auth.routes

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import AuthController from "../controllers/auth.controller.js";
+import AuthService from "../services/auth.service.js";
+import userRepository from "../repositories/user.repository.js";
+import tokenUtility from "../utils/jwt.utils.js";
+import emailService from "../infrastructure/email.service.js";
+import captchaService from "../infrastructure/captcha.service.js";
+
+const router = express.Router();
+
+const authService = new AuthService(
+  userRepository,
+  tokenUtility,
+  emailService,
+  captchaService,
+);
+
+const authController = new AuthController(authService);
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/verify-email", authController.verifyEmail);
+router.post("/refresh", authController.refreshToken);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword);
+
+export default router;
