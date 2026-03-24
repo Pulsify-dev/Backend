559add029e14e17a9f38af24634bafefeef7a694
Mohamed Ashraf <mkaramallah29@gmail.com>
2026-03-24 22:34:58 +0200

    fix(issues): fixed testing issues route endpoint and Mongoose Deprecation Warnings

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1", routes);
+app.use("/v1", routes);
 
 app.use(errorMiddleware.notFound);
 
